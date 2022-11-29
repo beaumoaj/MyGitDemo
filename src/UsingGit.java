0e3b49a65e126bcff82f372a26b66d71bdd8728f
beaumoaj <a.j.beaumont@aston.ac.uk>
2022-11-29 12:50:09 +0000

    Updated the main class

--- src/UsingGit.java
+++ src/UsingGit.java
@@ -4,6 +4,7 @@ public class UsingGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Using git");
+		System.out.println("Git is fun");
 	}
 
 }
